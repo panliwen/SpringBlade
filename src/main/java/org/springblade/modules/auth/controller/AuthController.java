5dd9d90d05e3c7e02d41970b99d0511eef066d96
panliwen <18933656486@163.com>
2019-12-14 09:22:01 +0800

    增加模板的配置

--- src/main/java/org/springblade/modules/auth/controller/AuthController.java
+++ src/main/java/org/springblade/modules/auth/controller/AuthController.java
@@ -46,11 +46,13 @@ public class AuthController {
 
 	@PostMapping("token")
 	@ApiOperation(value = "获取认证token", notes = "传入租户ID:tenantId,账号:account,密码:password")
-	public R<AuthInfo> token(@ApiParam(value = "授权类型", required = true) @RequestParam(defaultValue = "password", required = false) String grantType,
-							 @ApiParam(value = "刷新令牌") @RequestParam(required = false) String refreshToken,
-							 @ApiParam(value = "租户ID", required = true) @RequestParam(defaultValue = "000000", required = false) String tenantId,
+	public R<AuthInfo> token(@ApiParam(value = "租户ID", required = true) @RequestParam(defaultValue = "000000", required = false) String tenantId,
 							 @ApiParam(value = "账号") @RequestParam(required = false) String account,
 							 @ApiParam(value = "密码") @RequestParam(required = false) String password) {
+
+
+		String grantType = WebUtil.getRequest().getParameter("grant_type");
+		String refreshToken = WebUtil.getRequest().getParameter("refresh_token");
 
 		String userType = Func.toStr(WebUtil.getRequest().getHeader(TokenUtil.USER_TYPE_HEADER_KEY), TokenUtil.DEFAULT_USER_TYPE);
 
